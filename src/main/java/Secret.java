9bd8f6984c888ae465555b570407849a808d18d0
UnknownCoder 56 <67492611+UnknownCoder56@users.noreply.github.com>
2021-08-23 09:05:54 +0530

    ok

--- src/main/java/Secret.java
+++ src/main/java/Secret.java
@@ -1,8 +1,6 @@
 public class Secret {
 
-    private static String token = "ODc4ODQ5NTY2OTE5NzU3OTM1.YSHKcA.e5gnkRWh0CiCDShAEotWgj5q8GY";
-
     public static String getToken() {
-        return token;
+        return System.getenv("TOKEN");
     }
 }
